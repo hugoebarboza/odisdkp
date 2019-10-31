984c3da13e03d4d171ee33caf742396e473e104a
Hugo Barboza <hugoebarboza@gmail.com>
2019-10-30 22:13:43 -0300

    Update by Heroku Version

--- src/app/components/shared/kpi/kpi-project-heatday/kpi-project-heatday.component.ts
+++ src/app/components/shared/kpi/kpi-project-heatday/kpi-project-heatday.component.ts
@@ -5,9 +5,9 @@ import { FormControl } from '@angular/forms';
 import { PaymentService, UserService } from 'src/app/services/service.index';
 
 // AMCHARTS
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4charts from '@amcharts/amcharts4/charts';
-import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+// import * as am4core from '@amcharts/amcharts4/core';
+// import * as am4charts from '@amcharts/amcharts4/charts';
+// import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 
 
 
