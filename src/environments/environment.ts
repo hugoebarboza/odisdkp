7848fc364cca6a7e35d1e6ac9fd98b3d5dc6f4f0
Hugo Barboza <hugoebarboza@gmail.com>
2019-12-17 00:51:04 -0300

    Versión 68d9721

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: 'AIzaSyBPUJBehQOjnzd9S3iVkHk8drGUR-AXDKk',
     authDomain: 'odisdkp.firebaseapp.com',
